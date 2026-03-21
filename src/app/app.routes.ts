7ed6384351bbecb98a91268c424666a297d0e3ac
MATCHOUATHE <matchouathejhislaine@gmail.com>
2026-03-21 13:30:25 +0100

    tache effectuee: implementation login et  dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {Component} from '@angular/core';
+import {Login} from './pages/login/login';
+import {Dashboard} from './pages/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: Login},
+  {path:'home',component: Dashboard},
+];
---
+++ src/app/pages/dashboard/dashboard.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Haeder } from '../haeder/haeder';
+import { Navbar } from '../navbar/navbar';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [Haeder, Navbar],
+  templateUrl: './dashboard.html',
+  styleUrl: './dashboard.css',
+})
+export class Dashboard {
+
+}
